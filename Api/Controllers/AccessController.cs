1f849d41d0b340682a0ed6bc990a1347a6bc01dc
kasanchez3 <kasanchez3@outlook.com>
2022-12-15 08:50:01 -0600

    Add project files.

---
+++ Api/Controllers/AccessController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Results;
+
+namespace Api.Controllers
+{
+    public class AccessController : ApiController
+    {
+        [HttpGet]
+        public IHttpActionResult GetUsuarios() 
+        {
+
+            List<Usuario> list = new List<Usuario>();
+            using (Models.ApiEntities db = new Models.ApiEntities())
+            {
+                list = (from d in db.usuarios
+                        select new Usuario
+                        {
+                            Id = d.id,
+                            usuario = d.usuario1,
+                            contrasena = d.contrasena,
+                            estado = d.estado,
+                            token = d.token,
+                        }).ToList();
+            }
+
+            return Ok(list);
+        }
+
+        [HttpPost]
+        public IHttpActionResult ComprobarUsuarios([FromBody]Usuario user)
+        {
+            int respuesta = 0;
+            
+            using (Models.ApiEntities db = new Models.ApiEntities())
+            {
+               var list = db.usuarios.Where(x => x.usuario1 == user.usuario && x.contrasena == user.contrasena);
+
+                if (list.Count()>0)
+                {
+                    respuesta = 1;
+                }
+                else
+                {
+                    respuesta = 0;
+                }
+            }
+
+            return Ok(respuesta);
+        }
+
+      
+    }
+}
---
+++ Api/Usuario.cs
@@ -0,0 +1,12 @@
+﻿
+namespace Api
+{
+    public class Usuario
+    {
+        public int Id { get; set; }
+        public string usuario { get; set; }
+        public string contrasena { get; set; }
+        public string estado { get; set;}
+        public string token { get; set;}
+    }
+}
